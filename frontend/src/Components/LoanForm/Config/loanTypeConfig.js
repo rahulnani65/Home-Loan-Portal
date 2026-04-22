7d5f43a20d943a2cb669e8137ad60c3a120e99a8
rahulnani65 <rahulview65@gmail.com>
2026-04-22 16:03:24 +0530

    Add initial LoanForm module structure and basic setup

---
+++ frontend/src/Components/LoanForm/Config/loanTypeConfig.js
@@ -0,0 +1,7 @@
+export const LOAN_TYPES = {
+    PURSHASE:{
+        key:    "PURCHASE",
+        label:  "Home Purchase Loan",
+        steps: ["Basic","Employment","Financial","Property","Document"]
+    }
+}
