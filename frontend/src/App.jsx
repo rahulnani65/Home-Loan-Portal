e4d1847ce518d5bf9e8d903eb2fd4212e9aa55fa
Rakesh <2488213@cognizant.com>
2026-04-22 09:28:58 +0530

    frontend with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind v4 ✅ Working
+      </h1>
+    </div>
+ 
+
+    </>
+  )
+}
+
+export default App
